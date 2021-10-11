45d61f2dc3421679eace9681cd0d6154609de373
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-10-11 00:21:41 -0400

    heroku

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
-
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-
 const User = require("./models/User");
-
+const Tweet = require("./models/Tweet");
 const bodyParser = require("body-parser");
+const passport = require("passport")
 
-mongoose.connect(db, { useNewUrlParser: true})
-    .then(() => console.log("Connected to mongoDB"))
-    .catch(err => console.log(err));
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.get("/", (req, res) => {
-
-    res.send("Hello World!");
-});
-
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT ||  5000;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
